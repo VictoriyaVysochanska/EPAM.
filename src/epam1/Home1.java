db4428494b1e5024e87b29c4469e27656e11220b
VictoriyaVysochanska <victoriyavysochanska@gmail.com>
2019-02-16 20:38:54 +0200

    Initial commit

---
+++ src/epam1/Home1.java
@@ -0,0 +1,12 @@
+package epam1;
+import java.util.Scanner;
+
+public class Home1 {
+    public static void main(String []arg){
+       System.out.println("****  ****    *      *   *");
+       System.out.println("*     *  *   * *     ** **");
+       System.out.println("**    ****  *   *    * * *");
+       System.out.println("*     *    *******   *   *");
+       System.out.println("****  *   *       *  *   *");
+    }
+}
